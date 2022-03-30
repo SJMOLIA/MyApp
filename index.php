5b73f120e374321c0c9e9f698236886f4d92c8b4
SJMOLIA <37633367+SJMOLIA@users.noreply.github.com>
2022-03-30 12:58:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1><a href="page2.html">TTWELCOME TO WEB APP SERVICES</a></h1>
+<h1><a href="page2.html">WELCOME TO WEB APP SERVICES</a></h1>
 <?php
 
 echo "<h1 >his is my 1st Web App with azure</h1>";
